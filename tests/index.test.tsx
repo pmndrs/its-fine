ff43619111b6cfdc36261fc4f20c389578e58546
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-09-04 04:08:45 -0500

    chore: init package

---
+++ tests/index.test.tsx
@@ -0,0 +1,17 @@
+import { vi, it } from 'vitest'
+
+declare global {
+  var IS_REACT_ACT_ENVIRONMENT: boolean
+}
+
+// Let React know that we'll be testing effectful components
+global.IS_REACT_ACT_ENVIRONMENT = true
+
+// Mock scheduler to test React features
+vi.mock('scheduler', () => require('scheduler/unstable_mock'))
+
+// Silence react-dom & react-dom/client mismatch
+const logError = global.console.error.bind(global.console.error)
+global.console.error = (...args: any[]) => !args[0].startsWith('Warning') && logError(...args)
+
+it('runs without crashing', () => {})
