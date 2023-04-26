bbf58a2681214c76986d2d937b937e87a7676379
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-04-26 09:13:23 -0500

    fix: search alternate fibers

--- src/index.tsx
+++ src/index.tsx
@@ -94,18 +94,19 @@ export function useFiber(): Fiber<null> | undefined {
   // In development mode, React will expose the current component's Fiber as ReactCurrentOwner.
   // In production, we don't have this luxury and must traverse from FiberProvider via useId
   const id = React.useId()
-  const fiber = React.useMemo(
-    () =>
-      ReactCurrentOwner?.current ??
-      traverseFiber<null>(root, false, (node) => {
+  const fiber = React.useMemo(() => {
+    for (const _fiber of [ReactCurrentOwner?.current, root, root?.alternate]) {
+      if (!_fiber) continue
+      const fiber = traverseFiber<null>(_fiber, false, (node) => {
         let state = node.memoizedState
         while (state) {
           if (state.memoizedState === id) return true
           state = state.next
         }
-      }),
-    [root, id],
-  )
+      })
+      if (fiber) return fiber
+    }
+  }, [root, id])
 
   return fiber
 }
